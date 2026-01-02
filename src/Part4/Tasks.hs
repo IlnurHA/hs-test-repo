ad47b5936541ede3908dcfd522d8a6058be8b284
Ilnur <IlnurHA@mail.ru>
2026-01-02 18:10:27 +0300

    (applicative) functor and monad

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -45,5 +45,15 @@ instance Monoid (ReverseList a) where
     mempty = REmpty
     mconcat = foldl (<>) mempty
 instance Functor ReverseList where
+    fmap _ REmpty = REmpty
+    fmap f (xs :< x) = (fmap f xs) :< (f x)
 instance Applicative ReverseList where
+    pure x = REmpty :< x
+    (<*>) REmpty _ = REmpty
+    (<*>) (fs :< f) arr = (fs <*> arr) <> fmap f arr
 instance Monad ReverseList where
+    return = pure
+    arr >>= f = myFlatten (fmap f arr)
+        where
+            myFlatten REmpty = REmpty
+            myFlatten (xs :< x) = (myFlatten xs) <> x
