5f09d09b5a28bbca50cd7f1b789c2f16d0318668
Ilnur <IlnurHA@mail.ru>
2026-01-02 18:49:08 +0300

    task5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -15,20 +15,33 @@ myFoldr f res (x:xs) = f x $ myFoldr f res xs
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f arr = myFoldl (\x -> \y -> x ++ [f y]) [] arr
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myFoldl (\x -> \y -> x ++ (f y)) []
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myFoldl (\x -> \y -> x ++ y) [] 
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse arr = myFoldr (\x -> \y -> y ++ [x]) [] arr
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldr (\x -> \y -> (myFilterHelper p x) ++ y) []
+
+myFilterHelper :: (a -> Bool) -> a -> [a]
+myFilterHelper p x = case p x of
+    True -> [x]
+    False -> []
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
+myPartition p = myFoldr (\x -> \y -> partitionMerge (myPartitionHelper p x) y) ([], [])
+
+myPartitionHelper :: (a -> Bool) -> a -> ([a], [a])
+myPartitionHelper p x = case p x of
+    True -> ([x], [])
+    False -> ([], [x])
+
+partitionMerge :: ([a], [a]) -> ([a], [a]) -> ([a], [a])
+partitionMerge (x1, y1) (x2, y2) = (x1 ++ x2, y1 ++ y2)
 
