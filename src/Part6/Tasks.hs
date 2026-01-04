08ba87ffccabb5171c5d1c0a463bea8290fdef6f
Ilnur <IlnurHA@mail.ru>
2026-01-04 09:20:31 +0300

    new implementation for task6

--- src/Part6/Tasks.hs
+++ src/Part6/Tasks.hs
@@ -15,107 +15,71 @@ data SparseMatrix a = SparseMatrix {
 -- которые нужны, чтобы реализовать функции, представленные ниже
 class Matrix mx where
        mxzero :: Int -> Int -> mx
-       -- Without values from provided coordinates
-       submatrix :: mx -> Int -> Int -> mx
-       size :: mx -> (Int, Int)
-       row :: mx -> Int -> [Int]
-       column :: mx -> Int -> [Int]
-       insert :: mx -> (Int, Int) -> Int -> mx
+       mxsubmx :: mx -> Int -> Int -> Maybe mx
+       
+       --
+       mxsize :: mx -> (Int, Int)
+
+       --
+       mxrow :: mx -> Int -> Maybe [Int]
+       mxcolumn :: mx -> Int -> Maybe [Int]
+       
+       --
+       mxinsert :: mx -> Int -> Int -> Int -> mx
+       mxget :: mx -> Int -> Int -> Maybe Int
 
 -- Определите экземпляры данного класса для:
 --  * числа (считается матрицей 1x1)
 --  * списка списков чисел
 --  * типа SparseMatrix, представленного выше
 instance Matrix Int where
-       mxzero _ _ = 0
+       mxzero 1 1 = 0
+       mxzero _ _ = error "Wrong dimensions. Expected 1 1"
 
-       -- submatrix :: Int -> Int -> Int -> Int
-       submatrix mx 0 0 = 0
-       submatrix mx _ _ = mx
-       
-       -- size :: Int -> (Int, Int)
-       size _ = (1, 1)
+       mxsubmx _ _ _ = Nothing
+       mxsize _ = (1, 1)
 
-       -- row :: Int -> Int -> [Int]
-       row mx 0 = [mx]
-       row mx _ = []
+       mxrow mx 0    = Just [mx]
+       mxrow _ _     =  Nothing
 
-       -- column :: Int -> Int -> [Int]
-       column mx 0 = [mx]
-       column mx _ = []
+       mxcolumn mx 0 = Just [mx]
+       mxcolumn _ _  = Nothing
 
-       insert _ (0, 0) value = value
-       insert mx _ _ = mx
+       mxinsert _ 0 0 value = value
+       mxinsert mx _ _ _ = mx
 
--- Length
-myLength :: [a] -> Int
-myLength = lengthHelper 0
+       mxget mx 0 0 = Just mx
+       mxget _ _ _ = Nothing
 
-lengthHelper :: Int -> [a] -> Int
-lengthHelper res [] = res
-lengthHelper res (_:xs) = lengthHelper (res + 1) xs
 
--- Exclude by index
-exclude :: [a] -> Int -> [a]
-exclude = excludeHelper 1
-
-excludeHelper :: Int -> [a] -> Int -> [a]
-excludeHelper _ [] _ = []
-excludeHelper x0 arr@(x:xs) x1
-              | x0 == x1 = xs
-              | x1 > x0 = arr
-              | otherwise = x : excludeHelper (x0 + 1) xs (x1)
-
--- Get by index
-getByIndex :: a -> [a] -> Int -> a
-getByIndex = getByIndexHelper 0
-
-getByIndexHelper :: Int -> a -> [a] -> Int -> a
-getByIndexHelper _ def [] _ = def
-getByIndexHelper x0 def arr@(x:xs) x1
-              | x0 == x1 = x
-              | x1 > x0 = def
-              | otherwise = getByIndexHelper (x0 + 1) def xs x1
+elementAt :: Int -> [a] -> Maybe a
+elementAt _ [] = Nothing
+elementAt 0 (x:_) = Just x
+elementAt n (_:xs) = elementAt (n - 1) xs
 
 instance Matrix [[Int]] where
-       mxzero x y = mxzeroHelper x y []
-              where
-                     mxzeroHelper 0 _ res = res
-                     mxzeroHelper x y res = mxzeroHelper (x - 1) y ((zerorow y []) : res)
+       mxzero x y = replicate x (replicate y 0)
+       
+       mxsubmx mx x y = Just $ [[elem | (elem, yIndex) <- zip row [0..], yIndex /= y] | (row, xIndex) <- zip mx [0..], xIndex /= x]
 
-                     zerorow 0 res = res
-                     zerorow y res = zerorow (y - 1) (0:res)
-       -- submatrix :: [[Int]] -> Int -> Int -> [[Int]]
-       submatrix arr x y = Prelude.map (\subarr -> exclude subarr y) (exclude arr x)                     
+       mxsize [] = (0, 0)
+       mxsize arr@([]:xs) = (length arr, 0)
+       mxsize arr@(x:xs) = (length arr, length x)
 
-       -- size :: [[Int]] -> (Int, Int)
-       size [] = (0, 0)
-       size arr@([]:xs) = (myLength arr, 0)
-       size arr@(x:xs) = (myLength arr, myLength x)
+       mxrow mx x = elementAt x mx
+       mxcolumn mx y = Just $ [ elem | Just elem <- (Prelude.map (elementAt y) mx) ]
 
-       -- row :: [[Int]] -> Int -> [Int]
-       row = getByIndex []
-
-       -- column :: [[Int]] -> Int -> [Int]
-       column arr y = Prelude.map (\x -> getByIndex 0 x y) arr
-
-       -- insert :: [[Int]] -> (Int, Int) -> Int -> [[Int]]
-       insert mx (x, y) newValue = new_mx
-              where
-                     new_mx = insertHelper 0 mx x (\row -> insertHelper 0 row y (\_ -> newValue))
-
-                     insertHelper :: Int -> [a] -> Int -> (a -> a) -> [a]
-                     insertHelper _ [] _ _ = []
-                     insertHelper index arr@(x:xs) targetIndex f
-                            | index == targetIndex = (f x) : xs
-                            | index > targetIndex = arr
-                            | otherwise = x : (insertHelper (index + 1) xs targetIndex f)
+       mxinsert mx x y newValue = [[ chooseValue value newValue xIndex yIndex | (value, yIndex) <- zip row [0..]] | (row, xIndex) <- zip mx [0..]]
+              where chooseValue oldValue newValue xIndex yIndex
+                     | xIndex == x && yIndex == y = newValue
+                     | otherwise = oldValue
+       
+       mxget mx x y = elementAt x mx >>= elementAt y
 
 instance Matrix (SparseMatrix Int) where
        mxzero x y = SparseMatrix { sparseMatrixWidth=y, sparseMatrixHeight=x, sparseMatrixElements=Data.Map.empty }
 
-       -- submatrix :: SparseMatrix -> Int -> Int -> SparseMatrix
-       submatrix mx x y = SparseMatrix { sparseMatrixWidth=yMax - 1, sparseMatrixHeight=xMax - 1, sparseMatrixElements=yMoved $ xMoved elems }
+       mxsubmx mx x y = Just $ SparseMatrix { sparseMatrixWidth=yMax - 1, sparseMatrixHeight=xMax - 1, sparseMatrixElements=newElems }
               where
                      elems :: Map (Int, Int) Int
                      elems = sparseMatrixElements mx 
@@ -124,75 +88,65 @@ instance Matrix (SparseMatrix Int) where
                      yMax :: Int
                      yMax = sparseMatrixWidth mx
 
-                     xMoved :: Map (Int, Int) Int -> Map (Int, Int) Int
-                     xMoved mx = moveValues x xMax yMax constructKeyY $ exclude constructKeyX x yMax mx
-                     yMoved :: Map (Int, Int) Int -> Map (Int, Int) Int
-                     yMoved mx = moveValues y yMax xMax constructKeyX $ exclude constructKeyY y xMax mx
+                     newElems :: Map (Int, Int) Int
+                     newElems = moveKeys (deleteElems elems x y) x y
 
-                     constructKeyX :: Int -> Int -> (Int, Int)
-                     constructKeyX x = \yIndex -> (x, yIndex)
-                     constructKeyY :: Int -> Int -> (Int, Int)
-                     constructKeyY y = \xIndex -> (xIndex, y)
+                     deleteElems :: Map (Int, Int) Int -> Int -> Int -> Map (Int, Int) Int
+                     deleteElems mx x y = deleteHelper indecies mx
+                            where
+                                   indecies = indeciesToDelete x y
 
-                     exclude :: (Int -> Int -> (Int, Int)) -> Int -> Int -> Map (Int, Int) Int -> Map (Int, Int) Int
-                     exclude = excludeHelper 0
+                                   deleteHelper [] mx = mx
+                                   deleteHelper (x:xs) mx = deleteHelper xs (delete x mx)
 
-                     excludeHelper :: Int -> (Int -> Int -> (Int, Int)) -> Int -> Int -> Map (Int, Int) Int -> Map (Int, Int) Int
-                     excludeHelper index constructKey fixedIndex maxIndex mx 
-                                   | index >= maxIndex = mx
-                                   | otherwise = excludeHelper (index + 1) constructKey fixedIndex maxIndex (delete (constructKey fixedIndex index) mx)
+                     indeciesToDelete :: Int -> Int -> [(Int, Int)]
+                     indeciesToDelete x y = Prelude.foldl (<>) [] [[ (xIndex, yIndex) | yIndex <- [0..yMax], isNecessaryIndex yIndex xIndex ] | xIndex <- [0..xMax]]
+                            where
+                                   isNecessaryIndex xIndex yIndex = xIndex == x || yIndex == y
+                     
+                     moveKeys :: Map (Int, Int) Int -> Int -> Int -> Map (Int, Int) Int
+                     moveKeys mx x y = mapKeys moveK mx
+                            where 
+                                   moveK (xIndex, yIndex)
+                                          | xIndex > x && yIndex > y = (xIndex - 1, yIndex - 1)
+                                          | xIndex > x = (xIndex - 1, yIndex)
+                                          | yIndex > y = (xIndex, yIndex - 1)
+                                          | otherwise = (xIndex, yIndex)
 
-                     moveValues :: Int -> Int -> Int -> (Int -> Int -> (Int, Int)) -> Map (Int, Int) Int -> Map (Int, Int) Int
-                     moveValues = moveValuesHelper 0
-
-                     moveValuesHelper :: Int -> Int -> Int -> Int -> (Int -> Int -> (Int, Int)) -> Map (Int, Int) Int -> Map (Int, Int) Int
-                     moveValuesHelper curOtherIndex fromIndex toIndex maxOtherIndex constructKey mx
-                                   | curOtherIndex >= maxOtherIndex = mx
-                                   | otherwise = moveValuesHelper (curOtherIndex + 1) fromIndex toIndex maxOtherIndex constructKey $ moveValuesLine fromIndex constructKey curOtherIndex toIndex mx
-
-                     moveValuesLine :: Int -> (Int -> Int -> (Int, Int)) -> Int -> Int -> Map (Int, Int) Int -> Map (Int, Int) Int
-                     moveValuesLine index constructKey fixedIndex maxIndex mx
-                            | index >= maxIndex = mx
-                            | member old_index mx = moveValuesLine (index + 1) constructKey fixedIndex maxIndex new_mx
-                                   where
-                                          old_index = constructKey fixedIndex index
-                                          new_index = constructKey fixedIndex (index - 1)
-                                          value = mx ! old_index
-                                          new_mx = Data.Map.insert new_index value $ delete old_index mx
-
-       -- size :: SparseMatrix Int -> (Int, Int)
-       size mx = (xMax, yMax)
+       mxsize mx = (xMax, yMax)
               where
                      xMax = sparseMatrixHeight mx
                      yMax = sparseMatrixWidth mx
 
-       -- row :: SparseMatrix Int -> Int -> [Int]
-       row mx x = getByIndexSparse elems (x, 0) moveByY $ overflowed $ Part6.Tasks.size mx
+       -- mxrow :: mx -> Int -> Maybe [a]
+       mxrow mx x 
+              | x >= xMax || x < 0 = Nothing
+              | otherwise = Just $ [findWithDefault 0 (x, yIndex) elems | yIndex <- [0..yMax]]
               where
-                     moveByY (x, y) = (x, y + 1)
+                     xMax = sparseMatrixHeight mx
+                     yMax = sparseMatrixWidth mx
                      elems = sparseMatrixElements mx
 
-       -- column :: SparseMatrix Int -> Int -> [Int]
-       column mx y = getByIndexSparse elems (0, y) moveByX $ overflowed $ Part6.Tasks.size mx
+       -- mxcolumn :: mx -> Int -> Maybe [a]
+       mxcolumn mx y 
+              | y >= yMax || y < 0 = Nothing
+              | otherwise = Just $ [findWithDefault 0 (xIndex, y) elems | xIndex <- [0..xMax]]
               where
-                     moveByX (x, y) = (x + 1, y)
+                     xMax = sparseMatrixHeight mx
+                     yMax = sparseMatrixWidth mx
                      elems = sparseMatrixElements mx
        
-       -- insert :: SparseMatrix Int -> (Int, Int) -> Int -> SparseMatrix Int
-       insert mx coords@(x, y) newValue = SparseMatrix { sparseMatrixWidth=yMax, sparseMatrixHeight=xMax, sparseMatrixElements=newElems }
+       -- mxinsert :: mx -> Int -> Int -> a -> mx
+       mxinsert mx x y value
+              | x >= xMax || x < 0 || y >= yMax || y < 0 = mx
+              | otherwise = SparseMatrix { sparseMatrixWidth=yMax, sparseMatrixHeight=xMax, sparseMatrixElements=(insert (x, y) value elems) }
               where
-                     newElems = Data.Map.insert coords newValue $ sparseMatrixElements mx
-                     (xMax, yMax) = Part6.Tasks.size mx
-
-
-overflowed :: (Int, Int) -> (Int, Int) -> Bool
-overflowed (xMax, yMax) (x, y) = xMax <= x || yMax <= y
-
-getByIndexSparse :: Map (Int, Int) Int -> (Int, Int) -> ((Int, Int) -> (Int, Int)) -> ((Int, Int) -> Bool) -> [Int]
-getByIndexSparse mx index iterateF indexOverflowedCheck
-       | indexOverflowedCheck index = []
-       | otherwise = (findWithDefault 0 index mx) : getByIndexSparse mx (iterateF index) iterateF indexOverflowedCheck
-
+                     xMax = sparseMatrixHeight mx
+                     yMax = sparseMatrixWidth mx
+                     elems = sparseMatrixElements mx
+       
+       -- mxget :: mx -> Int -> Int -> Maybe a
+       mxget mx x y = (sparseMatrixElements mx) !? (x, y)
 
 -- Реализуйте следующие функции
 -- Единичная матрица
@@ -201,11 +155,11 @@ eye w = eyeHelper (mxzero w w) 0 w
        where
               eyeHelper :: Matrix m => m -> Int -> Int -> m
               eyeHelper mx curIndex maxIndex
-                     | curIndex >= maxIndex = mx
-                     | otherwise = eyeHelper (Part6.Tasks.insert mx (curIndex, curIndex) 1) (curIndex + 1) maxIndex
+                     | curIndex > maxIndex = mx 
+                     | otherwise = eyeHelper (mxinsert mx curIndex curIndex 1) (curIndex + 1) maxIndex
 -- Матрица, заполненная нулями
 zero :: Matrix m => Int -> Int -> m
-zero w h = mxzero w h
+zero w h = mxzero h w
 -- Перемножение матриц
 multiplyMatrix :: Matrix m => m -> m -> m
 multiplyMatrix = notImplementedYet
